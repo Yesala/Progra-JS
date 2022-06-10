7e76054a1c26c4479a42d681da8f1511226be4ca
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-06-09 23:05:01 -0600

    Semana2-Tarea3-Function-Upgrade

--- Semana 2/Tarea3-Function/js/main.js
+++ Semana 2/Tarea3-Function/js/main.js
@@ -6,17 +6,18 @@ function init() {
     var secondNumberIn = document.getElementById('secondNumber');
     var mostrarResultadoBtn = document.getElementById('mostrarResultadoBtn');
     mostrarResultadoBtn.onclick = onMostrarResultadoBtn;
-
-    var firstNumber = Number(firstNumberIn.value);
-    var secondNumber = Number(secondNumberIn.value);
+    var resultadoLbl = document.getElementById('resultadoLbl');
 
     function onMostrarResultadoBtn() {
+        var firstNumber = Number(firstNumberIn.value);
+        var secondNumber = Number(secondNumberIn.value);
+        
         if(firstNumber == secondNumber){
-            return "Los numeros son iguales"
+            resultadoLbl.innerHTML = "Los números son iguales";
         }else if (firstNumber > secondNumber){
-            return document.write("Número máximo: "+ firstNumber);
-        }else if (secondNumber > firstNumber){
-            return document.write("Número máximo: "+ secondNumber);
+            resultadoLbl.innerHTML = 'Número máximo: ' + firstNumber;
+        }else{
+            resultadoLbl.innerHTML = 'Número máximo: ' + secondNumber;
         }
     }
 }
