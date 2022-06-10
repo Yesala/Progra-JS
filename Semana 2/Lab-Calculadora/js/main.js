c8f2af98e6c0fbd900a455afee3cc34f0dda4910
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-06-09 20:46:43 -0600

    Semana2-Lab-Calculadora-v.1

--- Semana 2/Lab-Calculadora/js/main.js
+++ Semana 2/Lab-Calculadora/js/main.js
@@ -1,7 +1,8 @@
+"use strict";
+
 function calc() {
     var n1 = parseFloat(document.getElementById('n1').value);
     var n2 = parseFloat(document.getElementById('n2').value);
-
     var oper = document.getElementById('operators').value;
 
     if (oper === '+') {
@@ -16,8 +17,14 @@ function calc() {
         document.getElementById('result').value = n1 / n2;
     }
 
-    if (oper === 'X') {
+    if (oper === 'x') {
         document.getElementById('result').value = n1 * n2;
     }
 }
+
+
+
+
+
+
 
