fe3b094c38ab33b58819f2e005bba5415f34b275
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-06-17 19:42:09 -0600

    Semana 2-Lab-Calculadora-Correciones

--- Semana 2/Lab-Calculadora/js/main.js
+++ Semana 2/Lab-Calculadora/js/main.js
@@ -1,29 +1,40 @@
 "use strict";
+window.onload = init;
 
-function calc() {
-    var n1 = parseFloat(document.getElementById('n1').value);
-    var n2 = parseFloat(document.getElementById('n2').value);
-    var oper = document.getElementById('operators').value;
+function init(){
+    var calcBtn = document.getElementById('calcBtn');
+    var clearBtn = document.getElementById('clearBtn');
+    var result = document.getElementById('result');
+    var operSlt = document.getElementById('operators');
 
-    if (oper === '+') {
-        document.getElementById('result').value = n1 + n2;
+    clearBtn.onclick = onClr;
+    calcBtn.onclick = onCalc;
+
+    function onCalc() {
+        var n1 = parseFloat(document.getElementById('n1').value);
+        var n2 = parseFloat(document.getElementById('n2').value);
+        var oper = operSlt.value;
+    
+        if (oper === '+') {
+            result.value = n1 + n2;
+        }
+    
+        if (oper === '-') {
+            result.value = n1 - n2;
+        }
+    
+        if (oper === '/') {
+            result.value = n1 / n2;
+        }
+    
+        if (oper === 'x') {
+            result.value = n1 * n2;
+        }
     }
-
-    if (oper === '-') {
-        document.getElementById('result').value = n1 - n2;
+    
+    function onClr() {
+        document.getElementById("n1").value = ""
+        document.getElementById("n2").value = ""
+        result.value = ""
     }
-
-    if (oper === '/') {
-        document.getElementById('result').value = n1 / n2;
-    }
-
-    if (oper === 'x') {
-        document.getElementById('result').value = n1 * n2;
-    }
-}
-
-function clr() {
-    document.getElementById("n1").value = ""
-    document.getElementById("n2").value = ""
-    document.getElementById("result").value = ""
 }
