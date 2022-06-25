f514d83dd129a38863e6c2f49e6335522a965016
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-06-24 18:12:23 -0600

    Semana-4-Tarea-1

--- Semana 4/Tarea-1/main.js
+++ Semana 4/Tarea-1/main.js
@@ -5,10 +5,11 @@ window.addEventListener('load', init, false);
 function init() {
     var dailyTemp = [30,35,38,40,29,33,39];
     var totalTemp = 0;
+    var average = 0;
 
     for(let i =0; i < dailyTemp.length; i++){
         totalTemp += dailyTemp[i];
+        average = (totalTemp / dailyTemp.length).toFixed(1);
     }
-
-    console.log('El promedio de temperatura semanal es: ' + (totalTemp / dailyTemp.length).toFixed(1) + ' grados');
+    console.log('El promedio de temperatura semanal es: ' + average + ' grados');
 }
