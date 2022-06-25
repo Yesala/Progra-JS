31490edd4e6214c3ad2f8b593a00bd5f1a5b79fd
Yesala <ysalazarj@ucenfotec.ac.cr>
2022-06-24 18:12:37 -0600

    Semana-4-Tarea-2

--- Semana 4/Tarea-2/main.js
+++ Semana 4/Tarea-2/main.js
@@ -5,10 +5,12 @@ window.addEventListener('load', init, false);
 function init() {
     var dailySales = [30000,67000,124900,230785,155000,189050];
     var totalSales = 0;
+    var total = 0;
 
     for(let i =0; i < dailySales.length; i++){
         totalSales += dailySales[i];
+        total = (totalSales / dailySales.length).toFixed(2);
     }
 
-    console.log('El promedio de ventas diario es: ¢' + (totalSales / dailySales.length).toFixed(2) +'\n'+ 'y el total semanal de 6 días es: ¢' + totalSales);
+    console.log('El promedio de ventas diario es: ¢' + total +'\n'+ 'y el total semanal de ventas es: ¢' + totalSales);
 }
